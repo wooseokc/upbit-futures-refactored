8b535fc18c333eacb5ba5013af095d680572da60
Wooseok <wschun95@naver.com>
2022-11-23 03:16:59 +0900

    필요없어진 redux 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import GlobalStyle from './styles/global'
 import Header from './components/Header';
 import Main from './components/InfoSector';
-import store from './stores/coinStore';
-
-import { Provider } from "react-redux";
 
 function App() {
   return (
     <> 
       <GlobalStyle />
-      <Provider store={store}>
         <Header />
         <Main/>
-      </Provider>
     </>
-  );
+  )
 }
 
 export default App;
--- src/hooks/hooks.tsx
+++
@@ -1,8 +0,0 @@
-import type { TypedUseSelectorHook } from "react-redux";
-import { useDispatch, useSelector } from 'react-redux'
-
-
-import type { RootState, AppDispatch } from "../stores/coinStore";
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
